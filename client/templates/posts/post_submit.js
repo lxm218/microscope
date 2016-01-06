1eb3854117ae252d041a604e92fbb7c205eec84c
Lan <lxm218@gmail.com>
2016-01-05 22:45:22 -0800

    change

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -18,6 +18,7 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
     var errors = validatePost(post);
     if(errors.title || errors.url)
       return Session.set ('postSubmitErrors', errors);
